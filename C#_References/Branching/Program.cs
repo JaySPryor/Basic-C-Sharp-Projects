9992f55e82d7eae48953008ec642edde02fa5079
JayP <Jaymessp@gmail.com>
2021-08-24 15:01:33 -0600

    Branching ref & Shipping app

---
+++ C#_References/Branching/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Branching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("What is your favorite number?");
+            int favNum = Convert.ToInt32(Console.ReadLine());
+
+            //Ternary (?:) operator
+            string result = favNum == 12 ? "You have an awesome favorite number!" : "You don't have an awesome favorite number...";
+            Console.WriteLine(result);
+
+            //int roomTemperature = 70;
+
+            //Console.WriteLine("Hi, what is your name?");
+            //string name = Console.ReadLine();
+
+            //Console.WriteLine("Hi, " + name + ", what is the temp where you are?");
+            //int currentTemperature = Convert.ToInt32(Console.ReadLine());
+
+            //if (currentTemperature == roomTemperature)
+            //{
+            //    Console.WriteLine("It is exactly room temp.");
+            //}
+            //else if (currentTemperature > roomTemperature)
+            //{
+            //    Console.WriteLine("It is warmer than room temp.");
+            //}
+            //else if (roomTemperature > currentTemperature)
+            //{
+            //    Console.WriteLine("It is colder than room temp.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("uh oh...");
+            //}
+
+            //int currentTemp = 70;
+            //int roomTemp = 69;
+
+            //string comparisonResult = currentTemp == roomTemp ? "It is room temp." : "It is not room temp.";
+            //Console.WriteLine(comparisonResult);
+
+            //if (currentTemp == roomTemp)
+            //{
+            //    Console.WriteLine("It is exactly room temp.");
+            //}
+            //else if (currentTemp > roomTemp)
+            //{
+            //    Console.WriteLine("It is warmer than room temp.");
+            //}
+            //else if (roomTemp > currentTemp)
+            //{
+            //    Console.WriteLine("It is colder than room temp.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("It is NOT exactly room temp.");
+            //}
+            Console.ReadLine();
+        }
+    }
+}
