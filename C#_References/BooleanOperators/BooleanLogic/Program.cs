6fe62036d3b50f035623c632f64b975bda02dd1c
JayP <Jaymessp@gmail.com>
2021-08-23 19:45:48 -0600

    Boolean Logic

--- C#_References/BooleanOperators/BooleanLogic/Program.cs
+++ C#_References/BooleanOperators/BooleanLogic/Program.cs
@@ -10,6 +10,32 @@ namespace BooleanLogic
     {
         static void Main(string[] args)
         {
+            //// && (and)
+            //Console.WriteLine(true && false);
+            //Console.WriteLine(true && true);
+            //Console.WriteLine(false && false);
+
+            //// || (or)
+            //Console.WriteLine(true || false);
+            //Console.WriteLine(true || true);
+            //Console.WriteLine(false || false);
+
+            //// == (is equal too)
+            //Console.WriteLine(true == false);
+            //Console.WriteLine(true == true);
+            //Console.WriteLine(false == false);
+
+            //// != (not equal)
+            //Console.WriteLine(true != false);
+            //Console.WriteLine(true != true);
+            //Console.WriteLine(false != false);
+
+            // ^ (xor (true if one of the arguments is true, but not both) )
+            Console.WriteLine(true ^ false);
+            Console.WriteLine(true ^ true);
+            Console.WriteLine(false ^ false);
+
+            Console.ReadLine();
         }
     }
 }
