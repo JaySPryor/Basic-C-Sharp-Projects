f717c45d74e98caf91a80299df99262f57ddc90f
JayP <Jaymessp@gmail.com>
2021-08-25 17:55:19 -0600

    Created Iteration reference

--- C#_References/Iteration/Program.cs
+++ C#_References/Iteration/Program.cs
@@ -18,15 +18,69 @@ class Program
         //}
         //Console.ReadLine();
 
-        string[] names = { "Jaymes", "Erik", "Daniel", "Adam" };
+        //string[] names = { "Jaymes", "Erik", "Daniel", "Adam" };
 
-        for (int j = 0; j < names.Length; j++)
+        //for (int j = 0; j < names.Length; j++)
+        //{
+        //    if (names[j] == "Jaymes")
+        //    {
+        //        Console.WriteLine(names[j]);
+        //    }
+        //}
+
+        ////To print whole array to console:
+        //for (int j = 0; j < names.Length; j++)
+        //{
+        //    Console.WriteLine(names[j]);
+        //}
+        //Console.ReadLine();
+
+
+        //List<int> testScores = new List<int>();
+        //testScores.Add(98);
+        //testScores.Add(99);
+        //testScores.Add(78);
+        //testScores.Add(86);
+        //testScores.Add(85);
+
+        //foreach (int score in testScores)
+        //{
+        //    if (score > 85)
+        //    {   
+        //        Console.WriteLine("Passing test Score: " + score);
+        //    } 
+        //}
+        //Console.ReadLine();
+
+        /*
+        List<string> names = new List<string> { "Jaymes", "Erik", "Adam", "Daniel" };
+
+        foreach (string name in names)
+        {   
+            ////to print 1 item in list
+            //if (name == "Jaymes")
+            //{   
+            //    Console.WriteLine(name);
+            //}
+
+            //to print all items in list
+            Console.WriteLine(name);
+        }
+        */
+
+        List<int> testScores = new List<int>() { 98, 99, 12, 74, 23, 99 };
+        List<int> passingScores = new List<int>();
+
+        foreach (int score in testScores)
         {
-            if (names[j] == "Jaymes")
+            if (score > 85)
             {
-                Console.WriteLine(names[j]);
+                passingScores.Add(score);
             }
         }
+
+        Console.WriteLine(passingScores.Count);
+
         Console.ReadLine();
     }
 }
