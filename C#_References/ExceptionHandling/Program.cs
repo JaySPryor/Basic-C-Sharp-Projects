a284475694aca685ab1f56ff8fa438d8d51eef6a
JayP <Jaymessp@gmail.com>
2021-08-31 21:53:39 -0600

    Exception Handling

---
+++ C#_References/ExceptionHandling/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Pick a number");
+                int numberOne = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Pick another number");
+                int numberTwo = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Dividing the two...");
+                int numberThree = numberOne / numberTwo;
+
+                Console.WriteLine(numberOne + " divided by " + numberTwo + " equals " + numberThree);
+                Console.ReadLine();
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Please type a whole number.");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Cannot divide by zero(0)");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
