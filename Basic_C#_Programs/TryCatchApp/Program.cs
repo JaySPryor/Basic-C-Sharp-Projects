a5ea7715dace1cf4e7043ecc0ae1e9bad1339eca
JayP <Jaymessp@gmail.com>
2021-08-31 22:33:20 -0600

    Try/Catch Demo

---
+++ Basic_C#_Programs/TryCatchApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatchApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                List<int> numList = new List<int>() { 3, 6, 9 };
+
+                Console.WriteLine("Number?");
+                int userNum = Convert.ToInt32(Console.ReadLine());
+
+                foreach (int num in numList)
+                {
+                    int quotient = num / userNum;
+                    Console.WriteLine(num + " divided by " + userNum + " equals " + quotient);
+                }
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+
+            Console.WriteLine("\nYou have exited the try/catch block.");
+            Console.ReadLine();
+
+        }
+    }
+}
