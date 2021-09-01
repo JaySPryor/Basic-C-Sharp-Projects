c1e9443e472393e7483cb1e5371106c5cc1f200c
JayP <Jaymessp@gmail.com>
2021-08-31 23:10:18 -0600

    Comments for Try/Catch app

--- Basic_C#_Programs/TryCatchApp/Program.cs
+++ Basic_C#_Programs/TryCatchApp/Program.cs
@@ -10,30 +10,31 @@ namespace TryCatchApp
     {
         static void Main(string[] args)
         {
-
+            //start of try/catch block
             try
             {
+                //List of integers
                 List<int> numList = new List<int>() { 3, 6, 9 };
-
+                //request for user to input a number
                 Console.WriteLine("Number?");
-                int userNum = Convert.ToInt32(Console.ReadLine());
-
+                int userNum = Convert.ToInt32(Console.ReadLine());//Convert input to int
+                //loop that checks each element(num) in numList
                 foreach (int num in numList)
                 {
-                    int quotient = num / userNum;
+                    int quotient = num / userNum;//variable to hold quotient of list element divided by user input
                     Console.WriteLine(num + " divided by " + userNum + " equals " + quotient);
                 }
             }
-            catch (FormatException ex)
+            catch (FormatException ex)//first catch block, selected if user inputs anything besides whole number
             {
-                Console.WriteLine(ex);
+                Console.WriteLine(ex);//displays Format Exception error message
             }
-            catch (DivideByZeroException ex)
+            catch (DivideByZeroException ex)//secomd catch block, selected if user inputs 0
             {
-                Console.WriteLine(ex);
+                Console.WriteLine(ex);//displays Divide By Zero Exception error message
             }
 
-
+            //message displayed after exiting the try/catch block
             Console.WriteLine("\nYou have exited the try/catch block.");
             Console.ReadLine();
 
