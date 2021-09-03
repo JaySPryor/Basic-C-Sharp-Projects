fd2ae08305d7aa850497b2573306e4d66897173b
JayP <Jaymessp@gmail.com>
2021-09-02 23:04:19 -0600

    Method, optional parameter

---
+++ Basic_C#_Programs/Method/Operation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method
+{
+    public class Operation
+    {
+        int userInt1 { get; set; }
+        int optionalInt { get; set; }
+
+        public static int multiTool(int userInt, int optionalInt = 2)
+        {
+            return userInt * optionalInt;
+        }
+    }
+}
---
+++ Basic_C#_Programs/Method/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            Console.WriteLine("Type a number.");
+            int userInt = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Type a second number to multiply by the first, OR press enter to automatically double the first number.");
+            string userString = Console.ReadLine();
+            if (userString == null || userString == "0")
+            {
+                int result = Operation.multiTool(userInt);
+                Console.WriteLine(userInt + " times 2 equals " + result);
+            }
+            else
+            {
+                int optionalInt = Convert.ToInt32(userString);
+                int result = Operation.multiTool(userInt, optionalInt);
+                Console.WriteLine(userInt + " times " + optionalInt + " equals " + result);
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
