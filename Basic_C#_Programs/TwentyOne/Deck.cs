704a933f13e9bceb8302db1975eb9351c6cb41ea
JayP <Jaymessp@gmail.com>
2021-09-03 17:14:10 -0600

    moved shuffle method to deck class
    placed shuffle method inside of deck class.
    cleaned up the code, changed shuffle method to void type, added some comments

--- Basic_C#_Programs/TwentyOne/Deck.cs
+++ Basic_C#_Programs/TwentyOne/Deck.cs
@@ -8,6 +8,7 @@ namespace TwentyOne
 {
     public class Deck
     {
+        //Constructor (builds an object Deck of 52 cards, 4 different suits of 13 different faces
         public Deck()
         {
             Cards = new List<Card>();
@@ -28,10 +29,27 @@ namespace TwentyOne
                     Cards.Add(card);
                 }
             }
-
         }
 
+        //Properties of Deck object
         public List<Card> Cards { get; set; }
+        
+        //Shuffle method, shuffles deck, can be ran as many "times" as needed
+        public void Shuffle(int times = 1)
+        {
+            for (int i = 0; i < times; i++)
+            {
+                List<Card> TempList = new List<Card>();
+                Random random = new Random();
 
+                while (Cards.Count > 0)
+                {
+                    int randomIndex = random.Next(0, Cards.Count);
+                    TempList.Add(Cards[randomIndex]);
+                    Cards.RemoveAt(randomIndex);
+                }
+                Cards = TempList;
+            }
+        }
     }
 }
