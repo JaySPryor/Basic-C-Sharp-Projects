2b147796b531662b14115776cc488fcccd0fb453
JayP <Jaymessp@gmail.com>
2021-09-02 23:35:19 -0600

    Added comments

--- Basic_C#_Programs/Method/Operation.cs
+++ Basic_C#_Programs/Method/Operation.cs
@@ -8,12 +8,14 @@ namespace Method
 {
     public class Operation
     {
+        //Attributes
         int userInt1 { get; set; }
         int optionalInt { get; set; }
 
+        //Method that takes two integers and multiplies them, OR one integer and multiplies by 2 
         public static int multiTool(int userInt, int optionalInt = 2)
         {
-            return userInt * optionalInt;
+            return userInt * optionalInt;//returns int result
         }
     }
 }
--- Basic_C#_Programs/Method/Program.cs
+++ Basic_C#_Programs/Method/Program.cs
@@ -10,22 +10,31 @@ namespace Method
     {
         static void Main(string[] args)
         {   
-            
+            /*
+             *-------------!ATTENTION!---------------
+             *If user types a non integer or enters a blank 
+             *answer, a format exception is thrown. 
+             */
+
+            //Request for first number
             Console.WriteLine("Type a number.");
-            int userInt = Convert.ToInt32(Console.ReadLine());
+            int userInt = Convert.ToInt32(Console.ReadLine());//converted from string and saved as int userInt 
 
+            //Request for second number, or to use default
             Console.WriteLine("Type a second number to multiply by the first, OR type 0 to automatically double the first number.");
-            string userString = Console.ReadLine();
+            string userString = Console.ReadLine();//string saved 
 
+            //selected if user typed 0
             if (userString == "0")
             {
-                int result = Operation.multiTool(userInt);
-                Console.WriteLine(userInt + " times 2 equals " + result);
+                int result = Operation.multiTool(userInt);//passes userInt to method and uses default value (2) for optionalInt, returns result
+                Console.WriteLine(userInt + " times 2 equals " + result);//displays result 
             }
+            //selected if user typed any other number
             else
             {
-                int optionalInt = Convert.ToInt32(userString);
-                int result = Operation.multiTool(userInt, optionalInt);
+                int optionalInt = Convert.ToInt32(userString);//converts user input to int, saves as optionalInt
+                int result = Operation.multiTool(userInt, optionalInt);//Passes userInt & optionalInt to multiTool(), returns result
                 Console.WriteLine(userInt + " times " + optionalInt + " equals " + result);
             }
 
