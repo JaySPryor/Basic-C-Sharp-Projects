770f7a478f33567feeb684f00498dd9056db778a
JayP <Jaymessp@gmail.com>
2021-09-03 19:52:25 -0600

    Unfinished assignment

---
+++ Basic_C#_Programs/VoidMethod/Operation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    public class Operation
+    {
+        int a { get; set; }
+        int quotient { get; set; }
+
+        public void Divide(int a)
+        {
+            quotient = a / 2;
+        }
+
+    }
+}
---
+++ Basic_C#_Programs/VoidMethod/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Operation divide = new Operation();
+
+            Console.WriteLine("Type a number.");
+            int userInt = Convert.ToInt32(Console.ReadLine());
+
+            int result = divide.Divider(userInt);
+
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+
+        }
+    }
+}
