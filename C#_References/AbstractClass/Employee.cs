213c709131a07dc5e17b10ca2ebd6182f499e29d
JayP <Jaymessp@gmail.com>
2021-09-04 20:59:44 -0600

    Created overloaded == operator

--- C#_References/AbstractClass/Employee.cs
+++ C#_References/AbstractClass/Employee.cs
@@ -9,6 +9,8 @@ namespace AbstractClass
     //Inherits from Person class
     public class Employee : Person, IQuittable
     {
+        public int ID { get; set; } 
+
         //implementation of abstract method from Person class (use override keyword)
         public override string SayName()
         {
@@ -24,5 +26,20 @@ namespace AbstractClass
             //display message to console
             Console.WriteLine(employee + " has terminated employment.");
         }
+
+        //overloaded == operator
+        public static bool operator== (Employee employee1, Employee employee2)//takes two Employee objects
+        {
+            bool sameId = employee1.ID == employee2.ID;//Compares ID property of each, saves true/false as sameId
+            return sameId;//returns bool sameId
+        }
+
+        //overloaded != required for Employee== to exist
+        public static bool operator!= (Employee employee1, Employee employee2)
+        {
+            bool notSameId = employee1.ID != employee2.ID;
+            return notSameId;
+        }
+
     }
 }
