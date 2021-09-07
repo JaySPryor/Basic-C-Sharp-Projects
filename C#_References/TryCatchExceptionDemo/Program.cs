cd89a14cfbaa7a59bd915cda86e067c69ecb2568
JayP <Jaymessp@gmail.com>
2021-09-07 13:57:10 -0600

    Simple exception handling demo using try/catch

--- C#_References/TryCatchExceptionDemo/Program.cs
+++ C#_References/TryCatchExceptionDemo/Program.cs
@@ -10,6 +10,35 @@ namespace TryCatchExceptionDemo
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("How old are you?");
+
+            try
+            {
+                int userAge = Convert.ToInt32(Console.ReadLine());
+                DateTime currentDate = DateTime.Now;
+                DateTime userBirthDate = currentDate.AddYears(-userAge);
+                Console.WriteLine("You were born on {0}", userBirthDate);
+                Console.ReadLine();
+            }
+            
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Please enter a whole number, with no decimals.");
+                Console.ReadLine();
+                throw;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Please enter a whole number, not letters.");
+                Console.ReadLine();
+                throw;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Something went wrong.");
+                Console.ReadLine();
+                throw;
+            }
         }
     }
 }
