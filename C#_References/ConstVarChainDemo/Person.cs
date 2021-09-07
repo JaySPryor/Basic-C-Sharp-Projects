df737bcab441e08aa1b5390c5a474cc854e6ae43
JayP <Jaymessp@gmail.com>
2021-09-06 20:49:29 -0600

    Demo of: Cont/Var/Chained constructors

---
+++ C#_References/ConstVarChainDemo/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstVarChainDemo
+{
+    class Person
+    {
+        //Chained constructor
+        public Person(string name) : this(name, 100)
+        {
+        }
+
+        //Original constructor
+        public Person(string name, int wallet)
+        {
+            Name = name;
+            Wallet = wallet;
+        }
+
+        //Person Properties
+        public string Name { get; set; }
+        public int Wallet { get; set; }
+    }
+}
---
+++ C#_References/ConstVarChainDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstVarChainDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your name?");
+            //Save user input & create new Person as yourName (uses chained constructor in Person class)
+            Person yourName = new Person(Console.ReadLine());
+
+            //Displays user name and preset wallet ammount
+            Console.WriteLine("Hello {0}! You have {1} credits in your wallet.", yourName.Name, yourName.Wallet);
+
+            const string myName = "Jay P";//saves my name as a const string
+            var myWallet = 200;//use of var do declare int variable
+
+            Console.WriteLine("My name is {0}, and I have {1} credits in my wallet!", myName, myWallet);
+            Console.ReadLine();
+
+
+
+            
+
+        }
+    }
+}
