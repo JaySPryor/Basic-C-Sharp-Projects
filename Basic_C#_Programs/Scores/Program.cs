4a95c44232076c01ca4761a3f91e7334e6276f32
JayP <Jaymessp@gmail.com>
2021-09-10 19:50:17 -0600

    First .NET Core console app
    simple app that:
    takes data from a text file,
    displays it,
    displays the average.

---
+++ Basic_C#_Programs/Scores/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Scores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your first name.");
+            string date = DateTime.Today.ToShortDateString();
+            string uName = Console.ReadLine();
+            string msg = $"\nWelcome back {uName}. Today  is {date}.";
+            Console.WriteLine(msg);
+
+            string path = @"D:\School\GitHub\Basic-C-Sharp-Projects\Basic_C#_Programs\Scores\studentScores.txt";
+            string[] lines = System.IO.File.ReadAllLines(path);
+
+            double tScore = 0.0;
+
+            Console.WriteLine("\n Student Scores: \n");
+            foreach (string line in lines)
+            {
+                Console.WriteLine("\n" + line);
+                double score = Convert.ToDouble(line);
+                tScore += score;
+            }
+
+            double avgScore = tScore / lines.Length;
+            Console.WriteLine("\nAverage score from " + lines.Length + " student scores:\t" + avgScore);
+
+            Console.WriteLine("\n\nPress Enter to exit.");
+            Console.ReadLine();
+        }
+    }
+}
