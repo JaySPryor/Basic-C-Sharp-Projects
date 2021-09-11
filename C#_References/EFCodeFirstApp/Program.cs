e0acbfbccb1a5b65c66e33d54618ab45fcbe032c
JayP <Jaymessp@gmail.com>
2021-09-11 14:31:18 -0600

    Demo of an entity framework Code-First console app

---
+++ C#_References/EFCodeFirstApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EFCodeFirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var ctx = new SchoolContext())
+            {
+                var stud = new Student() { StudentName = "Jay" };
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
